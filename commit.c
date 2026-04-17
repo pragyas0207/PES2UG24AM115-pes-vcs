e3395ecd131b2d16a312d5a0abf5885c8d7b7d2a
pragyas0207 <pragyasriramaneni@gmail.com>
2026-04-17 11:56:35 +0530

    Phase 4: integrated tree creation from index

--- commit.c
+++ commit.c
@@ -197,6 +197,9 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
 
     ObjectID tree_id;
 
+ObjectID parent_id;
+    int has_parent = (head_read(&parent_id) == 0);
+
     if (tree_from_index(&tree_id) != 0) {
         return -1;
     }
